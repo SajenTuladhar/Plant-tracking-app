774d8c2f4d06f971a790d5c99111714eb45f3ab4
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-03-26 01:14:50 +0545

    try new layout

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,124 +14,151 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.black,),
+      
       body: ListView(
-        padding: const EdgeInsets.only(top: 20, bottom: 20),
+        
+        padding: EdgeInsets.zero,
         children: [
+          SizedBox(height: 50,),
           Container(
+                
             
             decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/plants.jpg'),
-                    fit: BoxFit.cover),
-
-                // ignore: prefer_const_literals_to_create_immutables
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromARGB(255, 177, 177, 177),
-                    spreadRadius: 1,
-                    blurRadius: 4,
-                    blurStyle: BlurStyle.outer,
-                  )
-                ],
-                color: Color.fromARGB(255, 255, 255, 255),
+              boxShadow:[BoxShadow(
+            blurStyle: BlurStyle.outer,
+            spreadRadius: 1,
+            blurRadius: 4,
+            color: Colors.black
+              )],
+              image: DecorationImage(
+              image: AssetImage('assets/images/plants.jpg'),
+                fit: BoxFit.cover),
                 borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(20),
                     topRight: Radius.circular(20),
-                    bottomLeft:Radius.circular(20),
-                    bottomRight: Radius.circular(20))),
-            padding: const EdgeInsets.all(12),
-            height: 200,
-            child: const Text(
-              "Good Morning \n User \n\n Your plants need water ! ",
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                fontSize: 23,
-                color: Colors.black,
-              ),
+                    bottomRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20)),
+                color: Color.fromARGB(255, 41, 200, 128)),
+            child: const Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                ListTile(
+                  
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                  title: Text(
+                    "Good Morning !",
+                    style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text(
+                    "User",
+                    style: TextStyle(color: Color.fromARGB(255, 0, 0, 0), fontSize: 25,fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
             ),
-          ),
-          SizedBox(
-            child: Container(
-              padding:const  EdgeInsets.only(top: 20),
-              color: Color.fromARGB(255, 255, 255, 255),
-              height: 20,
-            ),
-          ),
-          Container(
-            padding:const  EdgeInsets.only(top: 20),
-            color: Color.fromARGB(255, 225, 200, 200),
-            height: 200,
           )
         ],
       ),
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        title: const Text("Botany"),
-        elevation: 0,
-        centerTitle: true,
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton.large(
-        onPressed: () {
-          DialougeBox;
-        },
-        backgroundColor: const Color.fromARGB(255, 12, 172, 9),
-        elevation: 0,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(67)),
-        child: const Icon(
-          Icons.add,
-          size: 45,
-          color: Colors.white,
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        padding: const EdgeInsets.only(bottom: 2),
-        height: 70,
-        color: const Color.fromARGB(255, 184, 88, 16),
-        notchMargin: 0.1,
-        shape: const CircularNotchedRectangle(),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 45),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.home_filled),
-                    onPressed: () {},
-                    iconSize: 35,
-                    color: Colors.white,
-                    focusColor: Colors.white,
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 45),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.settings_rounded),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SettingsPage()),
-                      );
-                    },
-                    iconSize: 35,
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      // floatingActionButton: FloatingActionButton.large(
+      //   onPressed: () {
+      //     DialougeBox;
+      //   },
+      //   backgroundColor: const Color.fromARGB(255, 12, 172, 9),
+      //   elevation: 0,
+      //   shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(67)),
+      //   child: const Icon(
+      //     Icons.add,
+      //     size: 45,
+      //     color: Colors.white,
+      //   ),
+      // ),
+      // bottomNavigationBar: BottomAppBar(
+      //   padding: const EdgeInsets.only(bottom: 2),
+      //   height: 70,
+      //   color: const Color.fromARGB(255, 184, 88, 16),
+      //   notchMargin: 0.1,
+      //   shape: const CircularNotchedRectangle(),
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //     mainAxisSize: MainAxisSize.max,
+      //     children: [
+      //       Padding(
+      //         padding: const EdgeInsets.only(left: 45),
+      //         child: Column(
+      //           mainAxisSize: MainAxisSize.min,
+      //           children: [
+      //             IconButton(
+      //               icon: const Icon(Icons.home_filled),
+      //               onPressed: () {},
+      //               iconSize: 35,
+      //               color: Colors.white,
+      //               focusColor: Colors.white,
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //       Padding(
+      //         padding: const EdgeInsets.only(right: 45),
+      //         child: Column(
+      //           mainAxisSize: MainAxisSize.min,
+      //           children: [
+      //             IconButton(
+      //               icon: const Icon(Icons.settings_rounded),
+      //               onPressed: () {
+      //                 Navigator.push(
+      //                   context,
+      //                   MaterialPageRoute(
+      //                       builder: (context) => const SettingsPage()),
+      //                 );
+      //               },
+      //               iconSize: 35,
+      //               color: Colors.white,
+      //             ),
+      //           ],
+      //         ),
+      //       )
+      //     ],
+      //   ),
+      // ),
+      // body: Container(width: 406,
+      //       decoration: const BoxDecoration(
+
+      //           image: DecorationImage(
+      //               image: AssetImage('assets/images/plants.jpg'),
+      //               fit: BoxFit.cover),
+
+      //           // ignore: prefer_const_literals_to_create_immutables
+      //           boxShadow: [
+      //             BoxShadow(
+      //               color: Color.fromARGB(255, 177, 177, 177),
+      //               spreadRadius: 1,
+      //               blurRadius: 4,
+      //               blurStyle: BlurStyle.outer,
+      //             )
+      //           ],
+      //           color: Color.fromARGB(255, 255, 255, 255),
+      //           borderRadius: BorderRadius.only(
+      //               topLeft: Radius.circular(20),
+      //               topRight: Radius.circular(20),
+      //               bottomLeft:Radius.circular(20),
+      //               bottomRight: Radius.circular(20))),
+      //       padding: const EdgeInsets.all(12),
+      //       height: 200,
+      //       child: const Text(
+      //         "Good Morning \n User \n\n Your plants need water ! ",
+      //         textAlign: TextAlign.left,
+      //         style: TextStyle(
+      //           fontSize: 23,
+      //           color: Colors.black,
+      //         ),
+      //       ),
+      //     ),
     );
   }
 }
