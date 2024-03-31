82592db5743b0a04f71d7decbf5d1687c53556f8
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-04-01 00:14:12 +0545

    use of popscope

--- lib/pages/front_page.dart
+++ lib/pages/front_page.dart
@@ -1,4 +1,7 @@
+import 'package:botany/pages/home_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class FrontPage extends StatefulWidget {
   const FrontPage({Key? key}) : super(key: key);
@@ -26,15 +29,42 @@ class FrontPageState extends State<FrontPage> {
           children: [
             // Move the TextField down by 50 pixels
             Transform.translate(
-              offset: const Offset(0, 60),
-              child: const  TextField(
+              offset: const Offset(0, 155),
+              child: const TextField(
                 textAlign: TextAlign.left,
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
                   hintText: "Name",
                 ),
               ),
             ),
+            const SizedBox(
+              height: 180,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Homepage()),
+                  
+                );
+              },
+              // height: 40,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12), // <-- Radius
+                ),
+              ),
+              // textColor: Colors.white,
+              // color: const Color.fromARGB(255, 95, 186, 43),
+
+              child: const Text(
+                "Submit",
+                style: TextStyle(color: Colors.white),
+              ),
+            )
           ],
         ),
       ),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,176 +12,180 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(80),
-                    bottomLeft: Radius.circular(20)),
-                color: Color.fromARGB(255, 2, 200, 111)),
-            child: const Column(
-              children: [
-                SizedBox(
-                  height: 40,
-                ),
-                ListTile(
-                  contentPadding:
-                      EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-                  title: Text(
-                    "Good Morning !",
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text(
-                    "User",
-                    style: TextStyle(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold),
-                  ),
-                )
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          //Message container
-          UnconstrainedBox(
-            child: Container(
-              height: 200,
-              width: 385,
-              decoration: const BoxDecoration(
-                  color: Color.fromARGB(129, 71, 172, 66),
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                      topLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20))),
-              padding: const EdgeInsets.all(60),
-              child: const Text(
-                "Your Plants need water",
-                style: TextStyle(fontSize: 30),
-                textAlign: TextAlign.start,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-
-          //your plants container
-          
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            
-            children :[ Container(
-              
-              decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                      topLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20)),
-                  color: Color.fromARGB(255, 202, 201, 201)),
-              height: 600,
-              width: 385,
-              padding: const EdgeInsets.all(30),
-              
-              
-              child: Column(children: [
-              const Text("Your plants", 
-              style: TextStyle(fontSize: 25),textAlign: TextAlign.start),
-                 Expanded(
-                  child: GridView.builder( itemCount: 2,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), 
-                  itemBuilder: (context, index ){
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: 50  ,
-                        width: 50,
-                        color: Colors.blue,
-                      ),
-                    );
-                  })
-               
-            ,)
-
-              ],) 
-              
-            ),
-            ]
-            
-          )
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton.large(
-        onPressed: () {
-          DialougeBox;
-        },
-        backgroundColor: const Color.fromARGB(255, 2, 200, 111),
-        elevation: 0,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(67)),
-        child: const Icon(
-          Icons.add,
-          size: 45,
-          color: Colors.white,
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        padding: const EdgeInsets.only(bottom: 2),
-        height: 70,
-        color: const Color.fromARGB(255, 224, 115, 32),
-        notchMargin: 10,
-        shape: const CircularNotchedRectangle(),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          mainAxisSize: MainAxisSize.max,
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        
+        body: ListView(
+          padding: EdgeInsets.zero,
           children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 45),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
+            Container(
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(80),
+                      bottomLeft: Radius.circular(20)),
+                  color: Color.fromARGB(255, 2, 200, 111)),
+              child: const Column(
                 children: [
-                  IconButton(
-                    icon: const Icon(Icons.home_filled),
-                    onPressed: () {},
-                    iconSize: 35,
-                    color: Colors.white,
-                    focusColor: Colors.white,
+                  SizedBox(
+                    height: 40,
                   ),
+                  ListTile(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                    title: Text(
+                      "Good Morning !",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(
+                      "User",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  )
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(right: 45),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.settings_rounded),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const SettingsPage()),
-                      );
-                    },
-                    iconSize: 35,
-                    color: Colors.white,
-                  ),
-                ],
+            const SizedBox(
+              height: 30,
+            ),
+            //Message container
+            UnconstrainedBox(
+              child: Container(
+                height: 200,
+                width: 385,
+                decoration: const BoxDecoration(
+                    color: Color.fromARGB(129, 71, 172, 66),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                        topLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20))),
+                padding: const EdgeInsets.all(60),
+                child: const Text(
+                  "Your Plants need water",
+                  style: TextStyle(fontSize: 30),
+                  textAlign: TextAlign.start,
+                ),
               ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+      
+            //your plants container
+            
+      
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              
+              children :[ Container(
+                
+                decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
+                        topLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(20)),
+                    color: Color.fromARGB(255, 202, 201, 201)),
+                height: 600,
+                width: 385,
+                padding: const EdgeInsets.all(30),
+                
+                
+                child: Column(children: [
+                const Text("Your plants", 
+                style: TextStyle(fontSize: 25),textAlign: TextAlign.start),
+                   Expanded(
+                    child: GridView.builder( itemCount: 2,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), 
+                    itemBuilder: (context, index ){
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 50  ,
+                          width: 50,
+                          color: Colors.blue,
+                        ),
+                      );
+                    })
+                 
+              ,)
+      
+                ],) 
+                
+              ),
+              ]
+              
             )
           ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton.large(
+          onPressed: () {
+            DialougeBox;
+          },
+          backgroundColor: const Color.fromARGB(255, 2, 200, 111),
+          elevation: 0,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(67)),
+          child: const Icon(
+            Icons.add,
+            size: 45,
+            color: Colors.white,
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          padding: const EdgeInsets.only(bottom: 2),
+          height: 70,
+          color: const Color.fromARGB(255, 224, 115, 32),
+          notchMargin: 10,
+          shape: const CircularNotchedRectangle(),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 45),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.home_filled),
+                      onPressed: () {},
+                      iconSize: 35,
+                      color: Colors.white,
+                      focusColor: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 45),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.settings_rounded),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SettingsPage()),
+                        );
+                      },
+                      iconSize: 35,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
